3e10f0795fa1b281b5b2f845c7fd47add46f712d
Scott Leierer <scottleierer@gmail.com>
2024-11-15 16:07:15 -0500

    WIP

--- components/BuildSelect/BuildSelect.css.ts
+++ components/BuildSelect/BuildSelect.css.ts
@@ -30,6 +30,30 @@ export const ImageContainer = styled.div`
 export const Image = styled.img`
   width: 100%;
   border-radius: 0.5rem;
+  transition: all 1s ease;
+  box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.25);
+  position: relative;
+
+  ${({ selected }) =>
+    selected ? `position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: auto;
+    height: auto;
+    border-radius: 0;
+    z-index: 9999;` : null}
+`;
+
+export const BackgroundOverlay = styled.div`
+${({ visible }) =>
+  visible ? `position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 9998;` : null}
 `;
 
 export const Overlay = styled.div`
--- components/BuildSelect/index.tsx
+++ components/BuildSelect/index.tsx
@@ -1,7 +1,7 @@
 import React, { useMemo, useState } from "react";
 import { LinkButton } from "patterns";
 import Link from "next/link";
-import { BuildSelectContainer, GridContainer, ImageContainer, Image, Overlay, InfoContainer, InfoHeader, InfoText } from "./BuildSelect.css";
+import { BuildSelectContainer, GridContainer, ImageContainer, Image, Overlay, InfoContainer, InfoHeader, InfoText, BackgroundOverlay } from "./BuildSelect.css";
 
 export const BuildSelect = ({ builds }) => {
   const [selectedIndex, setSelectedIndex] = useState(null);
@@ -17,17 +17,18 @@ export const BuildSelect = ({ builds }) => {
 
   return (
     <BuildSelectContainer>
+      <BackgroundOverlay visible={selectedIndex != null} onClick={() => setSelectedIndex(null)} />
       <GridContainer>
         {builds.map((build, index) => (
           <ImageContainer key={build.id} onClick={() => handleClick(index)}>
-            <Image src={build.bannerImage.sizes.thumbnail.url} alt={build.bannerImage.alt} priority />
+            <Image selected={selectedIndex === index} src={build.bannerImage.sizes.thumbnail.url} alt={build.bannerImage.alt} priority />
             <Overlay visible={selectedIndex === index}><Link href={`/builds/${build.slug}`} /></Overlay>
           </ImageContainer>
         ))}
       </GridContainer>
       <InfoContainer>
         <InfoHeader>{buildDetails.title}</InfoHeader>
-        <InfoText>{buildDetails.shortDescription}</InfoText>
+        {selectedIndex != null ? <InfoText>{buildDetails.shortDescription}</InfoText> : null}
         {selectedIndex != null ? <LinkButton href={`/builds/${builds[selectedIndex].slug}`}>Read more</LinkButton> : null}
       </InfoContainer>
     </BuildSelectContainer>);
